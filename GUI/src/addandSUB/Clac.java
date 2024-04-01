a4af2ae5ab4f0633f3584378d1c5cfa19ff683f9
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-04-01 02:03:42 -0400

    radioDemo, Will select only one option and will display their name based on the option.

--- GUI/src/addandSUB/Clac.java
+++ GUI/src/addandSUB/Clac.java
@@ -11,60 +11,61 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class Clac {
-public static void main(String[] args) {
-	AddSub obj = new AddSub();
-}
+	public static void main(String[] args) {
+		AddSub obj = new AddSub();
+	}
 }
 
-
-class AddSub extends JFrame implements ActionListener{
-	//create the buttons
+class AddSub extends JFrame implements ActionListener {
+	// create the buttons
 	JTextField t1, t2;
-	JButton b1, b2;	
+	JButton b1, b2;
 	JLabel l;
-	
+
 	public AddSub() {
-		//Define each button
-		t1 =  new JTextField(15);
-		t2 =  new JTextField(15);
+		// Define each button
+		t1 = new JTextField(15);
+		t2 = new JTextField(15);
 		b1 = new JButton("ADD");
 		b2 = new JButton("SUB");
-		l =  new JLabel("Result");
-		
-		//Add the buttons
+		l = new JLabel("Result");
+
+		// Add the buttons
 		add(t1);
 		add(t2);
 		add(b1);
 		add(b2);
 		add(l);
-		
-		//add Action Listener
+
+		// add Action Listener
 		b1.addActionListener(this);
 		b2.addActionListener(this);
-		
-		//window settings 
-		setLayout(new FlowLayout()); 	//crtl + shift + i
-		setSize(400,400);
+
+		// window settings
+		setLayout(new FlowLayout()); // crtl + shift + i
+		setSize(400, 400);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
+
 	}
-	//import action event
-	public void actionPerformed(ActionEvent ae) {
-		int num1 = Integer.parseInt(t1.getText());
-		int num2 = Integer.parseInt(t2.getText());
-		
-		//logical
-		int value = 0;
-		
-		if(ae.getSource()==b1) 
-			value = num1 + num2;
-		
-		else 
-			value = num1 - num2;
-			
-		
-		l.setText(value + "");
-	}
+	
+	  //import action event
+	  
+	  public void actionPerformed(ActionEvent ae) { int num1 =
+	  Integer.parseInt(t1.getText()); int num2 = Integer.parseInt(t2.getText());
+	  
+	  //logic 
+	  int value = 0;
+	  
+	  //simple if else statement 
+	  if(ae.getSource()==b1) 
+		  value = num1 + num2;
+	  
+	  else value = num1 - num2;
+	 
+	 
+	  l.setText(value + ""); 
+	  }
+	 
+
 }
---
+++ GUI/src/jRadioButton/UserInput.java
@@ -0,0 +1,88 @@
+package jRadioButton;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.Flow;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+public class UserInput {
+	public static void main(String[] args) {
+		radioDemo r =  new radioDemo();
+	}
+}
+
+class radioDemo extends JFrame{
+	//JtextField
+	JTextField t1;
+	JButton b;
+	JRadioButton r1,r2;
+	JLabel l;
+	
+	
+	public radioDemo() {
+		t1 = new JTextField(15);
+		b = new JButton("ok");
+		r1 =  new JRadioButton("Male");
+		r2 =  new JRadioButton("Female");
+		l =  new JLabel("Greetings");
+		
+		//this only selects one radio button not both 
+		//by Grouping
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(r1);
+		bg.add(r2);
+		
+		
+		
+		//the order Matters 
+		//extends Jframe
+		add(t1);
+		add(r1);
+		add(r2);
+		add(b);
+		add(l);
+		
+		
+		
+		//action listener
+		b.addActionListener(new ActionListener() {
+			
+			//logic
+			public void actionPerformed(ActionEvent e) {
+				//t1 = JTextField
+				//will use Get Text and Set Text to modify
+				String name = t1.getText();
+				
+				//is selected method =  boolean value 
+				if (r1.isSelected()) {
+					name = "Mr. " + name;
+				}
+				else {
+					name = "Ms. " + name;
+				}
+				
+				//will display the name on the panel 
+				l.setText(name);
+			}
+			
+		});
+		
+		
+		
+		//window frame
+		setLayout(new FlowLayout());
+		setVisible(true);
+		setSize(400,400);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	
+}
